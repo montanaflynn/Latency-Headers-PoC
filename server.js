6a1bdc2530e65d673e8b6659d74cbf7eeff3a78f
Montana Flynn <montana@montanaflynn.me>
2014-10-17 06:54:02 -0700

    Ported koa to standard http module

--- server.js
+++ server.js
@@ -1,38 +1,30 @@
-var koa = require('koa')
-var app = koa()
- 
+// Load the http module
+var http = require('http');
+
 // Make it easy to pkill from npm
 process.title = "latencyPoC"
- 
-// Set the x-request-received header
-app.use(function *(next){
-  var timestamp = new Date().getTime()
-  this.set('x-request-received', timestamp)
-  yield next
-})
- 
-// Simulate processing time
-app.use(function *(next){
-  function process() {
-    var timeout = Math.floor((Math.random() * 50) + 10);
-    return function (cb) {
-      setTimeout(cb, timeout)
-    }
-  }
-  yield process()
-  yield next
-})
- 
-// Send the response
-app.use(function *(){
 
-  // Just for fun and the demo
-  this.set('x-powered-by', 'magic')
-  this.body = 'Hello World'
+// Create the server
+var server = http.createServer(function (request, response) {
 
-  // Set the x-response-sent header
-  var timestamp = new Date().getTime()
-  this.set('x-response-sent', timestamp)
-})
- 
-app.listen(1337)
+  // Set the x-request-received header with the current timestamp
+  response.setHeader('x-request-received', new Date().getTime());
+
+  // Simulate a delay between 50-100ms for processing latency
+  setTimeout(function(){
+
+    // Set the x-response-sent header with the current timestamp
+    response.setHeader('x-response-sent', new Date().getTime());
+
+    // Set the header status to ok and the content type
+    response.writeHead(200, {"Content-Type": "text/plain"});
+
+    // Return the obligatory hello world response
+    response.end("Hello World\n");
+
+  }, Math.floor((Math.random() * 100) + 50))
+
+});
+
+// Listen on localhost:1337
+server.listen(1337);
