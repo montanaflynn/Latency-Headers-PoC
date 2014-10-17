4eb0dc4aaf93c66c9cc343736d8b3a8e9a253948
Montana Flynn <montana@montanaflynn.me>
2014-10-17 01:11:15 -0700

    Error checking

--- client.js
+++ client.js
@@ -15,41 +15,48 @@ unirest
     var latencyBenchmark = benchmarkLatency(response)
     console.log(latencyBenchmark)
 
-    // Instead of console.log you could put it in a database
-    // db.logs.save(latencyBenchmark)
   })
 
 // The response handler to process the HTTP request latency
 function benchmarkLatency(response) {
-  if (response.status === 200) {
-    // Save the timestamp of when the response was received
-    var responseReceived = new Date().getTime()
 
-    // Save the headers set by the server
-    var requestReceived = response.headers['x-request-received']
-    var responseSent = response.headers['x-response-sent']
+  // Save the timestamp of when the response was received
+  var responseReceived = new Date().getTime()
 
-    // The math to determine the latencies
-    var outgoingLatency = requestReceived - requestSent
-    var processingLatency = responseSent - requestReceived
-    var incomingLatency = responseReceived - responseSent
-    var roundtripLatency = outgoingLatency + processingLatency + incomingLatency
-    
-    // Print out the latency in human readable format
-    console.log("=================================================")
-    console.log("Latency Benchmarks: ")
-    console.log("Total outgoing network latency: " + outgoingLatency + "ms")
-    console.log("Total processing time latency: " + processingLatency + "ms")
-    console.log("Total incoming network latency: " + incomingLatency + "ms")
-    console.log("Total round trip latency: " + roundtripLatency + "ms")
-    console.log("=================================================\n")
+  // There was an error with the request so we must stop here
+  if (response.error) {
+    return '{ "error" : "There was a problem connecting to the server" }'
+  }
 
-    // Return the latency in JSON so you log it, etc...
-    return {
-      "outgoing" : outgoingLatency + "ms",
-      "processing" : processingLatency + "ms",
-      "incoming" : incomingLatency + "ms",
-      "roundtrip" : roundtripLatency + "ms"
-    }
+  // Save the headers we care about in variables
+  var responseSent = response.headers['x-response-sent'] || false
+  var requestReceived = response.headers['x-request-received'] || false
+
+  // If the server is not sending the latency headers we stop here
+  if (!responseSent || !requestReceived){
+    return '{ "error" : "The server did not respond with latency headers" }'
+  }
+
+  // The math to determine the latencies
+  var outgoingLatency = requestReceived - requestSent
+  var processingLatency = responseSent - requestReceived
+  var incomingLatency = responseReceived - responseSent
+  var roundtripLatency = outgoingLatency + processingLatency + incomingLatency
+  
+  // Print out the latency in human readable format
+  console.log("=================================================")
+  console.log("Latency Benchmarks: ")
+  console.log("Total outgoing network latency: " + outgoingLatency + "ms")
+  console.log("Total processing time latency: " + processingLatency + "ms")
+  console.log("Total incoming network latency: " + incomingLatency + "ms")
+  console.log("Total round trip latency: " + roundtripLatency + "ms")
+  console.log("=================================================\n")
+
+  // Return the latency in JSON so you log it, etc...
+  return {
+    "outgoing" : outgoingLatency + "ms",
+    "processing" : processingLatency + "ms",
+    "incoming" : incomingLatency + "ms",
+    "roundtrip" : roundtripLatency + "ms"
   }
 }
