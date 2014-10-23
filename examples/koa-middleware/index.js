773754a37c483c7b7d7f3254b76312a1faac3bbe
Montana Flynn <montana@montanaflynn.me>
2014-10-22 20:28:58 -0700

    Update koa example

--- examples/koa-middleware/index.js
+++ examples/koa-middleware/index.js
@@ -3,22 +3,26 @@ var koa = require('koa')
 var app = koa()
 
 // Latency Header Middleware
-var latency = require('./koa-latency-headers')
-
-// Demo Middleware
-var demo = require('./demo')
+var latency = require('koa-latency-headers')
  
 // Set the x-request-received header
-app.use(latency.requestReceived)
+app.use(latency())
 
-// Simulate processing delay 
-app.use(demo.processingDelay)
+// Send the response body
+app.use(function *(next){
 
-// Create the demo response body
-app.use(demo.helloWorld)
- 
-// Set the x-response-sent header
-app.use(latency.responseSent)
+  // Simulate processing delay
+  yield (function() {
+    var timeout = Math.floor((Math.random() * 25) + 25);
+    return function (cb) {
+      setTimeout(cb, timeout)
+    }
+  })()
+
+  // Obligatory hello world
+  this.body = 'Hello World'
+  
+})
 
 // Listen for incoming requests
 app.listen(1337)
